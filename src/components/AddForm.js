b265a5e5ba8675b93db74d9b7099ec985ef13bca
frankafan <frank.a.fan@outlook.com>
2022-11-08 15:49:37 -0800

    style: use popup modal for add employee UI

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -6,6 +6,7 @@ const AddForm = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [salary, setSalary] = useState("");
+    const [dialogOpen, setDialogOpen] = useState(false);
 
     // Update backend employee data with employee data stored in the states
     const handleSubmit = () => {
@@ -13,7 +14,7 @@ const AddForm = () => {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
-              },          
+            },
             body: JSON.stringify({
                 firstName: firstName,
                 lastName: lastName,
@@ -27,22 +28,63 @@ const AddForm = () => {
         );
     };
 
+    function closeModal() {
+        setDialogOpen(false);
+    }
+
+    function openModal() {
+        setDialogOpen(true);
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                First Name:
-                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-            </label>
-            <label>
-                Last Name:
-                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-            </label>
-            <label>
-                Salary:
-                <input type="text" value={salary} onChange={(e) => setSalary(e.target.value)} />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+        <>
+            <button
+                className="bg-blue-400 hover:bg-blue-600 text-white font-bold m-5 mb-12 px-3 py-2 border border-gray-400 rounded shadow"
+                type="button"
+                onClick={() => setDialogOpen(true)}
+            >
+                Add Employee
+            </button>
+            {dialogOpen ? (
+                <>
+                    <div className="justify-center items-center flex fixed inset-0 z-50">
+                        <div className="bg-white shadow-lg rounded">
+                            <div className="p-5 border rounded-t">
+                                <h1 className="text-xl">
+                                    Edit Employee
+                                </h1>
+                            </div>
+                            <div className="py-5 px-14">
+                                <form onSubmit={handleSubmit}>
+                                    <label className="block text-lg font-light mt-4">
+                                        First Name:
+                                        <input className="ml-4 rounded border-gray-300 p-3 border" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                                    </label>
+                                    <label className="block text-lg font-light mt-4">
+                                        Last Name:
+                                        <input className="ml-4 rounded border-gray-300 p-3 border" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                                    </label>
+                                    <label className="block text-lg font-light mt-4">
+                                        Salary:
+                                        <input className="ml-4 rounded border-gray-300 p-3 border" type="text" value={salary} onChange={(e) => setSalary(e.target.value)} />
+                                    </label>
+                                    <input type="submit" value="Submit" className="bg-white hover:bg-gray-300 font-bold mt-5 mx-1 px-3 py-2 border border-gray-400 rounded shadow" />
+                                    <button
+                                        type="button"
+                                        className="bg-red-500 hover:bg-red-700 text-white font-bold mt-5 mx-1 px-3 py-2 border border-gray-400 rounded shadow"
+                                        onClick={closeModal}
+                                    >
+                                        Cancel
+                                    </button>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+        </>
     )
 
 }
