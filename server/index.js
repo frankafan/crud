d301049dc87a40830003c29d6eb241d90b09b863
frankafan <frank.a.fan@outlook.com>
2022-11-07 22:09:48 -0800

    fix: enable CORS

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const PORT = process.env.BACKEND_PORT || 8000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Connect to Mongo database
