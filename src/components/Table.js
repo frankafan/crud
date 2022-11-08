86dc18854d3d434ecfb38e4d9444037d481f62e1
frankafan <frank.a.fan@outlook.com>
2022-11-07 16:52:11 -0800

    fix: create and render simple table

---
+++ src/components/Table.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Table = () => {
+    return (
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Salary</th>
+                <th></th>
+            </tr>
+        </table>
+    )
+}
+
+export default Table;
