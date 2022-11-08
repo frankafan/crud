0f1e4cc758e7da7b6b84cb111c8c7f57033b1f59
frankafan <frank.a.fan@outlook.com>
2022-11-07 22:07:09 -0800

    fix: separate frontend port and backend port
    PORT now indicates frontend port, and BACKEND_PORT now indicates backend port.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.BACKEND_PORT || 8000;
 
 const app = express();
 app.use(express.json());
