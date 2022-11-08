4ef9be1a415b7bc451cbf15e0e8c00ace3588087
frankafan <frank.a.fan@outlook.com>
2022-11-08 01:25:04 -0800

    fix: handle submit button and update employee data
    Store employee data in states, and change them as the input form is changed. Update employee data with the stored states when submit button is clicked.

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -17,4 +17,45 @@ const EditForm = (employee) => {
     const [lastName, setLastName] = useState("");
     const [salary, setSalary] = useState("");
 
+    // Update backend employee data with employee data stored in the states
+    const handleSubmit = () => {
+        fetch("http://localhost:8000/employee/" + id, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+              },          
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                salary: salary
+            })
+        }
+        ).then(
+            res => res.json()
+        ).then(
+            res => console.log(res.message)
+        );
+    };
+
+    return (
+        // Initialize form values from employee data stored in states, and update states on form change
+        <form onSubmit={handleSubmit}>
+            <label>
+                First Name:
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </label>
+            <label>
+                Last Name:
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </label>
+            <label>
+                Salary:
+                <input type="text" value={salary} onChange={(e) => setSalary(e.target.value)} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    )
+
+}
+
 export default EditForm;
